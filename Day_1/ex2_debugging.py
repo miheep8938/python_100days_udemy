23ffd8fe9826caf9dd95d63785330314dfe33ca8
miheep8938 <miheep8938@gmail.com>
2023-09-26 18:34:37 +0900

    first commit

---
+++ Day_1/ex2_debugging.py
@@ -0,0 +1,17 @@
+#Fix the code below 👇
+
+#Check the parenthesis
+#print("Day 1 - String Manipulation"
+print("Day 1 - String Manipulation")
+
+#make sure you have + between strings to concat. 
+#print("String Concatenation is done with the + sign.") 
+print("String Concatenation is done with the "+ '"+"' + " sign.")
+
+#Usage of "" and ''
+#('e.g. print("Hello " + "world")')
+print('e.g. print("Hello " + "world")')
+
+# no indentation before print! 
+# print("New lines can be created with a backslash and n.")
+print("New lines can be created with a backslash and n.")
---
+++ Day_1/note.py
@@ -0,0 +1,28 @@
+###PRINTING###
+
+# Write your code below this line 👇
+
+#Create New Line '\n'
+print("Hello World!\nHello world!")
+
+
+#Create Spaces 
+print("Hello " + "Angela")
+print("Hello" +" "+ "Angela")
+print("Hello " + " Angela")
+print ("what is your name?")
+
+#Input: a prompt for the user - ready for the user to type in. 
+input("What is your name?")
+
+print("Hello " + input("What is your name?"))
+
+###VARIABLES###
+
+name = input("What is your name?")
+print(name)
+
+#Use len() to count characters 
+name = input("What is your name?")
+length = len(name)
+print(length)
